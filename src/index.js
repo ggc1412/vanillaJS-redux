6037933c0021bd8866f8714c2d60bc8636aad392
ggc1412 <ggc1412@gmail.com>
2020-04-25 00:59:22 +0900

    :hammer:refactoring

--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "add";
+const MINUS = "minus";
+// 오타가 났을 경우, 이를 확인하기 쉽게하기 위해 변수로 선언하여 사용한다.
+
 const countModifier = (count = 0, action) => {
-  console.log(action);
-  if (action.type === "add") {
-    count++;
-  } else if (action.type === "minus") {
-    count--;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return count;
 };
+// 가독성을 위해 일반적으로 if문보다 switch문을 사용한다.
 
 const store = createStore(countModifier);
 // store 안의 data를 변경하는 유일한 방법은 dispatch() 메서드를 사용하는 것이다.
@@ -21,11 +27,11 @@ const store = createStore(countModifier);
 store.subscribe(() => (number.innerHTML = store.getState()));
 
 const countAdd = () => {
-  store.dispatch({ type: "add" });
+  store.dispatch({ type: ADD });
 };
 
 const countMinus = () => {
-  store.dispatch({ type: "minus" });
+  store.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", countAdd);
