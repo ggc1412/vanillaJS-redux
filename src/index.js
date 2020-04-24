87a6340d023e0923c514c38e016749112d933f5f
ggc1412 <ggc1412@gmail.com>
2020-04-25 00:31:37 +0900

    :heavy_plus_sign:use subscribe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const countModifier = (count = 0, action) => {
+  console.log(action);
+  if (action.type === "add") {
+    count++;
+  } else if (action.type === "minus") {
+    count--;
+  }
+  return count;
+};
+
+const store = createStore(countModifier);
+// store 안의 data를 변경하는 유일한 방법은 dispatch() 메서드를 사용하는 것이다.
+// reducer는 현재 state에 주어진 action을 한 다음 state tree 값을 리턴하는 함수이다.
+
+store.subscribe(() => (number.innerHTML = store.getState()));
+
+const countAdd = () => {
+  store.dispatch({ type: "add" });
+};
+
+const countMinus = () => {
+  store.dispatch({ type: "minus" });
+};
+
+add.addEventListener("click", countAdd);
+minus.addEventListener("click", countMinus);
+number.innerHTML = store.getState();
