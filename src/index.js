709a4f722a1239ba1e0111fbfaa756f3e175416e
ggc1412 <ggc1412@gmail.com>
2020-04-26 22:22:37 +0900

    :cat2:React version start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import "./todoList";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+
+ReactDOM.render(<App></App>, document.getElementById("root"));
