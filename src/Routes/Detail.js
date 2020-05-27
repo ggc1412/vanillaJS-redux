f5a31064ae5b17bea9a3fd4d4432d499cf81ec48
ggc1412 <ggc1412@gmail.com>
2020-05-28 00:19:59 +0900

    :herb:detail에 삭제기능 추가

--- src/Routes/Detail.js
+++ src/Routes/Detail.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import { Link } from "react-router-dom";
 
-const Detail = ({ toDo }) => {
+const Detail = ({ toDo, onBtnClick }) => {
     return (
         <>
             <h1>{toDo?.text}</h1>
             <h5>Created at: {toDo?.id}</h5>
+            <Link to="/">
+                <button onClick={() => onBtnClick(toDo?.id)}>DEL</button>
+            </Link>
         </>
     )
 }
@@ -19,4 +24,10 @@ const mapStateToProps = (state, ownProps) => {
     return { toDo: state.find(toDo => toDo.id === parseInt(id))};
 }
 
-export default connect(mapStateToProps)(Detail);
+const mapDispatchToProps = (dispatch) => {
+    return {
+      onBtnClick: (id) => dispatch(actionCreators.deleteToDo(id))
+    };
+  }
+
+export default connect(mapStateToProps,mapDispatchToProps)(Detail);
